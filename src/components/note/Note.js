0b0f5a295f9d3691c3b917c339362597f24e4467
kvrp97 <ravinduprabhashwara97@gmail.com>
2023-04-04 12:06:28 +0530

    Note delete feature added

--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -5,8 +5,51 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Swal from 'sweetalert2'
+import axios from 'axios';
 
 export default function Note(props) {
+
+  const deleteNote = () => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios.delete('http://localhost:8090/api/v1/note/delete',{
+          params:{
+            noteId: props.id
+          }
+        })
+          .then((response) => {
+            console.log(response.data);
+            if (response.data === true){
+              Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+              )
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Something went wrong!',
+            })
+          })
+          .finally(function () {
+            
+          });
+      }
+    })
+  }
 
   return (
     <>
@@ -21,7 +64,7 @@ export default function Note(props) {
         </CardContent>
         <CardActions>
           <Button size="small">Edit</Button>
-          <Button size="small">Delete</Button>
+          <Button onClick={deleteNote} size="small">Delete</Button>
         </CardActions>
       </Card>
     </>
