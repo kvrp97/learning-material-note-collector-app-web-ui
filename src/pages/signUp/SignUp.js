e1789d8443459f59abb87433a60deae36e77997a
kvrp97 <ravinduprabhashwara97@gmail.com>
2023-04-07 19:19:01 +0530

    sign-up pages created & added styles

--- src/pages/signUp/SignUp.js
+++ src/pages/signUp/SignUp.js
@@ -1,8 +1,48 @@
 import React from 'react'
 import '../signUp/SignUp.css'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='container-sign-up'>
+      <div className='form-container-sign-up'>
+        <form className='sign-up-form'>
+          <h3>USER SIGN UP</h3>
+          <div className='user-name'>
+            <div className='name'>
+              <TextField required label="First Name" variant="outlined" />
+            </div>
+            <div className='name'>
+              <TextField required label="Last Name" variant="outlined" /> 
+            </div>
+          </div>
+          <br />
+          <div  className='em-pswd'>
+            <div>
+              <TextField
+                className='txt'
+                id="outlined-email-input"
+                label="Email Address"
+                type="email"
+                required
+              />
+            </div><br />
+            <div>
+              <TextField
+                className='txt'
+                id="outlined-password-input"
+                label="Password"
+                type="password"
+                required
+              />
+            </div>
+            <br />
+            <Button className='sign-up-btn' type='submit' variant="contained">Sign Up</Button>
+            <a className='login-link' href='/'>I have an account? LogIn</a>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
